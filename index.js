d10764e0f58a9249988c797f39a34c0e3945404a
Juraj J <jurajjukic.work@gmail.com>
2023-10-26 08:29:40 +0200

    Inicijalizacija lokalnog repozitorija

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/proba/:name", (request, response) => {
+    response.send("Hello, " + request.params.name); 
+});
+
+app.listen(port, ()=>{
+console.log("I am ready!");
+});
