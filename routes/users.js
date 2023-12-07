f115d05dc52fcac44353677215edfc491202fe0f
Juraj J <jurajjukic.work@gmail.com>
2023-12-07 11:26:26 +0100

    handlebars

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const joi = require ("joi");
+
+const { db } = require("../services/db.js");
 
 /* GET users listing. */
 router.get('/signin', function(req, res, next) {
+  res.render('users/signin');
+});
+
+const signinSchema = joi.object({
+  email: joi.string().email().max(50).required(),
+  password: joi.string().min(3).max(50).required()
+});
+
+router.post('/signin', function(req, res, next) {
+const result = signinSchema.validate(req.body);
+if (result.error){
+  res.sendStatus(400);
+}
+
+const email = req.body.email;
+const password = req.body.password;
+
+const stmt = db.prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+const dbResult = stmt.get(email, password);
+console.log("DB kaže", dbResult);
+
+
   res.render('users/signin');
 });
 
